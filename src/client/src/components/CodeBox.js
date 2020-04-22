aeea29e7dfbe9f43928e054ea3f7eed82e2308bf
DrVonDevious <drvondevious@gmail.com>
2020-04-22 16:10:35 -0500

    Add code boxes with inputs

--- src/client/src/components/CodeBox.js
+++ src/client/src/components/CodeBox.js
@@ -1,8 +1,64 @@
 import React from 'react';
 
+import 'codemirror/lib/codemirror.css';
+import 'codemirror/theme/material.css';
+
+import 'codemirror/mode/css/css';
+import 'codemirror/mode/javascript/javascript';
+import 'codemirror/mode/htmlmixed/htmlmixed';
+
+import {UnControlled as CodeMirror} from 'react-codemirror2'
+import { useSelector } from 'react-redux';
+
 const CodeBox = () => {
+
+  const mode = useSelector(state => state.code.mode);
+
   return (
-    <div></div>
+    <div className="code-box">
+
+      {/* --------- CSS Code Box -------- */}
+      { mode === "css" ? [
+        <div>
+          CSS:
+          <CodeMirror
+            options={{
+              mode: "css",
+              theme: "material",
+              lineNumbers: true
+            }}
+          />
+        </div>
+      ] : null }
+
+      {/* --------- HTML Code Box -------- */}
+      { mode === "html" ? [
+        <div>
+          HTML:
+          <CodeMirror
+            options={{
+              mode: "htmlmixed",
+              theme: "material",
+              lineNumbers: true
+            }}
+          />
+        </div>
+      ] : null }
+
+      {/* ------ Javascript Code Box ----- */}
+      { mode === "js" ? [
+        <div>
+          JavaScript:
+          <CodeMirror
+            options={{
+              mode: "javascript",
+              theme: "material",
+              lineNumbers: true
+            }}
+          />
+        </div>
+      ] : null }
+    </div>
   );
 };
 
--- src/client/src/containers/CodeBoxContainer.js
+++ src/client/src/containers/CodeBoxContainer.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { setMode } from '../reducers/codeReducer';
 
 import CodeBox from '../components/CodeBox';
 
 import '../css/CodeBox.css';
 
 const CodeBoxContainer = () => {
+
+  const dispatch = useDispatch();
+
   return (
     <div className="code-box-container">
+      <button onClick={() => dispatch(setMode("css"))}>CSS</button>
+      <button onClick={() => dispatch(setMode("html"))}>HTML</button>
+      <button onClick={() => dispatch(setMode("js"))}>JS</button>
       <CodeBox />
     </div>
   );
