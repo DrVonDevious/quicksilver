0e7d79b908cd0b79c9e6e7febaac15616a6c0f79
DrVonDevious <drvondevious@gmail.com>
2020-05-06 10:47:13 -0500

    Add build

---
+++ client/build/precache-manifest.9746b1e125e0301a1648011a68506bb1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf01561cf4d4d79151311088717d1765",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eeb894249d44e9da55b3",
+    "url": "/static/css/2.e681e74d.chunk.css"
+  },
+  {
+    "revision": "a79dba4acbcdba4109be",
+    "url": "/static/css/main.fad7fb27.chunk.css"
+  },
+  {
+    "revision": "eeb894249d44e9da55b3",
+    "url": "/static/js/2.11a117ff.chunk.js"
+  },
+  {
+    "revision": "4243871c0eb3312b6eb88998ddf5e244",
+    "url": "/static/js/2.11a117ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a79dba4acbcdba4109be",
+    "url": "/static/js/main.30da46d9.chunk.js"
+  },
+  {
+    "revision": "702554416a81a1e3a894",
+    "url": "/static/js/runtime-main.784c586e.js"
+  }
+]);
